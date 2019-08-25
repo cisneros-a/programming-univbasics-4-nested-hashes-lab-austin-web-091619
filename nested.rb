99ef32b116a78bc5b1bde4bbbb68505a42c7a227
Adrian Cisneros <cisneros_adrian51@yahoo.com>
2019-08-25 18:37:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,11 +15,11 @@ def hopper
           :languages => ["C"]
         }
      }
-     p programmer_hash[:grace_hopper]
+     programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
-	p programmer_hash[:alan_kay][:known_for]
+	programmer_hash[:alan_kay][:known_for]
 
 
 	programmer_hash =
