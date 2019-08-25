c62720b61d07eb3205de0074a448e95f83a14a76
Adrian Cisneros <cisneros_adrian51@yahoo.com>
2019-08-25 18:57:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -125,7 +125,9 @@ def adding_to_dennis
           :known_for => "Unix",
           :languages => ["C"]
         }
-     } programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+     } 
+
+     programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
      programmer_hash[:dennis_ritchie][:languages][1]
 
 end
